d3939ad24cb000c971390f3467b8394c44fe0e9f
Alok Kumar  Singh <aloksingh3112@gmail.com>
2018-12-30 15:10:53 +0530

    Add files via upload

---
+++ JS-December-Meetup-29-Dec-2018/webpack/src/footer.js
@@ -0,0 +1,10 @@
+
+import 'footer.css';
+
+const div1=document.createElement("div");
+div1.innerHTML="div 1";
+
+const footer=document.createElement("footer");
+footer.appendChild(div1);
+
+export {footer};
